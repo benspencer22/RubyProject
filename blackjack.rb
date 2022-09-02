58a242b7e5f4efea38425e157054e0533d6f9220
Ben Spencer <54424540+benspencer22@users.noreply.github.com>
2022-09-01 23:03:51 -0400

    added file to git with some variables defined

---
+++ blackjack.rb
@@ -0,0 +1,21 @@
+/*
+
+CS430 Ruby Project
+
+Author: Ben Spencer
+
+Simulated Blackjack Game
+
+
+*/
+
+/ Constants for the cards /
+
+CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+Dealer = [rand(11), rand[11]]
+
+
+
+
+
