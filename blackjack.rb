50dd2bfe538810a0544ce04744fd52c4e8e35f65
Ben Spencer <54424540+benspencer22@users.noreply.github.com>
2022-09-01 23:30:43 -0400

    added a method
    checks a players hand to see if he busted

--- blackjack.rb
+++ blackjack.rb
@@ -1,21 +1,39 @@
-/*
+#
+#
+# CS430 Ruby Project
+#
+# Author: Ben Spencer
+#
+# Simulated Blackjack Game
+#
+#
+#
 
-CS430 Ruby Project
-
-Author: Ben Spencer
-
-Simulated Blackjack Game
-
-
-*/
-
-/ Constants for the cards /
+#Constants for the cards
 
 CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
-Dealer = [rand(11), rand[11]]
+dealer = [rand(12-1), rand(12-1)]
+player = [rand(12-1), rand(12-1)]
 
+def checkHand(hand)
 
+	bust = false
+	
+	bustnum = hand[0] + hand[1]
+	
+	if bustnum > 21 then
+		puts "You busted!"
+	else
+		bust = true
+	end
+	
+end
+
+puts "%s %s" % [player[0], player[1]]
+puts "%s %s" % [dealer[0], dealer[1]]
+
+puts"Which action do you want to perform: Hit(H) Stay(S) 
 
 
 
